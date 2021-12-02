dac6d628a8d0fef9d117f26bb88eee40bb65ed52
UBJ <usamabinjafarpk335@gmail.com>
2021-12-02 14:11:39 +0530

    added login button

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -38,7 +38,7 @@ function Header() {
           <Arrow></Arrow>
         </div>
         <div className="loginPage">
-          <span>{user ? `Welcome ${user.displayName} `: 'Login'}</span>
+          <span onClick={()=>history.push("/login")}>{user ? `Welcome ${user.displayName} `: 'Login'}</span>
           <hr />
         </div>
           {user ? <span onClick={()=>{
